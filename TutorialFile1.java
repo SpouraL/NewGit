768f4b45156fe4be1ce26647f6c51282571cda8c
Aniket Udhlani <mehudon2809@gmail.com>
2020-04-23 20:15:30 +0530

    initial commit

---
+++ TutorialFile1.java
@@ -0,0 +1,13 @@
+package gitTutorial;
+
+public class TutorialFile1 {
+
+	public static void main(String[] args) {
+		System.out.println("Trying Git");
+		for(int i=1; i<9; i++) {
+			System.out.println("Hey");
+		}
+
+	}
+
+}
---
+++ TutorialFile2.java
@@ -0,0 +1,10 @@
+package gitTutorial;
+
+public class TutorialFile2 {
+
+	public static void main(String[] args) {
+		System.out.println("Continuing with git");
+
+	}
+
+}
